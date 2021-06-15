5f349ea6ac918cfbc235f04ed47d3431dde2a589
Nicolas Bouilleaud <nico@bou.io>
2021-06-15 16:31:12 +0200

    comments

--- app/mailers/concerns/ics_multipart_attached.rb
+++ app/mailers/concerns/ics_multipart_attached.rb
@@ -4,8 +4,16 @@ module IcsMultipartAttached
   def mail(*args)
     ics = ics_payload.present? ? IcalHelpers::Ics.from_payload(ics_payload) : nil
 
+    # Sending icalendar events by email is complicated, buggy and unreliable. This is the result of various hacks.
+    # * We add the icalendar twice to the email: once as a text/calendar part with a method, once as an attached .ics file.
+    # * The method is always PUBLISH: we don’t want any replies.
+    # There are other, historical tweaks that may not make sense:
+    # * the attachment is using `encoding: "8bit"` to “fix encoding issues in ICS” (?)
+    # * the text/calendar part is base64 encoded, although  “quoted-printable would be more adapted but there seems to be an encoding problem with extra =0D” (?)
+
+    # Attachment
     if ics.present?
-      message.attachments[ics_payload[:name]] = { # as an attachment
+      message.attachments[ics_payload[:name]] = {
         mime_type: "text/calendar",
         content: ics,
         encoding: "8bit" # not sure why
@@ -14,6 +22,7 @@ module IcsMultipartAttached
 
     message = super
 
+    # Separate part
     if ics.present?
       message.add_part( # and as a separate part
         Mail::Part.new do
